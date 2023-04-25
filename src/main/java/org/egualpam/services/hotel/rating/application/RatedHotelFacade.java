2565ae027f813b301e487197224adc5d7dfc1d41
Gualpa, Erick <erick.gualpa@sky.uk>
2023-04-25 20:40:17 +0200

    Fix build making RatedHotelFacade implementation of HotelService interface

--- src/main/java/org/egualpam/services/hotel/rating/application/RatedHotelFacade.java
+++ src/main/java/org/egualpam/services/hotel/rating/application/RatedHotelFacade.java
@@ -1,5 +1,18 @@
 package org.egualpam.services.hotel.rating.application;
 
-public class RatedHotelFacade {
-    // TODO: Add unit testing and implement!
+import java.util.ArrayList;
+import java.util.List;
+import org.egualpam.services.hotel.rating.controller.HotelQuery;
+import org.egualpam.services.hotel.rating.controller.HotelService;
+import org.egualpam.services.hotel.rating.domain.RatedHotel;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RatedHotelFacade implements HotelService {
+
+    @Override
+    public List<RatedHotel> findHotelsMatchingQuery(HotelQuery query) {
+        // TODO: Add unit testing and implement!
+        return new ArrayList<>();
+    }
 }
