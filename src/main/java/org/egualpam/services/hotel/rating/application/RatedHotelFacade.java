e06d2cc48f6d7189d18b8bd8d2979746dbc4500d
Gualpa, Erick <erick.gualpa@sky.uk>
2023-04-25 20:36:16 +0200

    Add testing and class skeletons for rated hotel facade

---
+++ src/main/java/org/egualpam/services/hotel/rating/application/RatedHotelFacade.java
@@ -0,0 +1,5 @@
+package org.egualpam.services.hotel.rating.application;
+
+public class RatedHotelFacade {
+    // TODO: Add unit testing and implement!
+}
---
+++ src/test/java/org/egualpam/services/hotel/rating/application/RatedHotelFacadeTest.java
@@ -0,0 +1,7 @@
+package org.egualpam.services.hotel.rating.application;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RatedHotelFacadeTest {
+    // TODO: Add unit testing
+}
