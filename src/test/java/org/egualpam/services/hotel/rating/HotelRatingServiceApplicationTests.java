8fa3cd6a985fe5870e215eb1bcb2cce74996f8ff
Gualpa, Erick <erick.gualpa@sky.uk>
2023-04-25 19:33:36 +0200

    Setup repository

---
+++ src/test/java/org/egualpam/services/hotel/rating/HotelRatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.egualpam.services.hotel.rating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelRatingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
