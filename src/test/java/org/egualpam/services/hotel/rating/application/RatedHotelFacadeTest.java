fe121b9c52d526a790bac250c05b73802d8c54ec
Gualpa, Erick <erick.gualpa@sky.uk>
2023-04-26 11:09:15 +0200

    Add failing test for hotel ordering based in rating average

--- src/test/java/org/egualpam/services/hotel/rating/application/RatedHotelFacadeTest.java
+++ src/test/java/org/egualpam/services/hotel/rating/application/RatedHotelFacadeTest.java
@@ -31,6 +31,10 @@ class RatedHotelFacadeTest {
     public static final String EXPECTED_REVIEW_IDENTIFIER = "AMZ_REVIEW";
     public static final int EXPECTED_REVIEW_RATING = 5;
     public static final String EXPECTED_REVIEW_COMMENT = "Eloquent comment";
+    public static final String EXPECTED_WORST_HOTEL_IDENTIFIER = "EXPECTED_WORST_HOTEL_IDENTIFIER";
+    public static final String EXPECTED_INTERMEDIATE_HOTEL_IDENTIFIER =
+            "EXPECTED_INTERMEDIATE_HOTEL_IDENTIFIER";
+    public static final String EXPECTED_BEST_HOTEL_IDENTIFIER = "EXPECTED_BEST_HOTEL_IDENTIFIER";
 
     @Mock private HotelRepository hotelRepository;
     @Mock private ReviewRepository reviewRepository;
@@ -44,14 +48,7 @@ class RatedHotelFacadeTest {
 
     @Test
     void givenAnyQuery_hotelsMatchingQueryShouldBeReturned() {
-        Hotel defaultHotel =
-                new Hotel(
-                        EXPECTED_HOTEL_IDENTIFIER,
-                        EXPECTED_NAME,
-                        EXPECTED_DESCRIPTION,
-                        EXPECTED_LOCATION,
-                        EXPECTED_TOTAL_PRICE,
-                        EXPECTED_IMAGE_URL);
+        Hotel defaultHotel = buildHotelStub(EXPECTED_HOTEL_IDENTIFIER);
 
         when(hotelRepository.findHotelsMatchingQuery(any(HotelQuery.class)))
                 .thenReturn(List.of(defaultHotel));
@@ -70,21 +67,9 @@ class RatedHotelFacadeTest {
 
     @Test
     void givenAnyQuery_hotelsMatchingQueryShouldBePopulatedWithReviewsAndReturned() {
-        Hotel defaultHotel =
-                new Hotel(
-                        EXPECTED_HOTEL_IDENTIFIER,
-                        EXPECTED_NAME,
-                        EXPECTED_DESCRIPTION,
-                        EXPECTED_LOCATION,
-                        EXPECTED_TOTAL_PRICE,
-                        EXPECTED_IMAGE_URL);
+        Hotel defaultHotel = buildHotelStub(EXPECTED_HOTEL_IDENTIFIER);
 
-        Review defaultReview =
-                new Review(
-                        EXPECTED_REVIEW_IDENTIFIER,
-                        EXPECTED_REVIEW_RATING,
-                        EXPECTED_REVIEW_COMMENT,
-                        EXPECTED_HOTEL_IDENTIFIER);
+        Review defaultReview = buildReviewStub(EXPECTED_REVIEW_RATING, EXPECTED_HOTEL_IDENTIFIER);
 
         when(hotelRepository.findHotelsMatchingQuery(any(HotelQuery.class)))
                 .thenReturn(List.of(defaultHotel));
@@ -100,5 +85,60 @@ class RatedHotelFacadeTest {
         assertThat(actualReview.getIdentifier()).isEqualTo(EXPECTED_REVIEW_IDENTIFIER);
         assertThat(actualReview.getRating()).isEqualTo(EXPECTED_REVIEW_RATING);
         assertThat(actualReview.getComment()).isEqualTo(EXPECTED_REVIEW_COMMENT);
+    }
+
+    @Test
+    void givenAnyQuery_hotelsMatchingQueryShouldBeReturnedOrderedByRatingAverage() {
+        Hotel expectedWorstHotel = buildHotelStub(EXPECTED_WORST_HOTEL_IDENTIFIER);
+        List<Review> expectedWorstHotelReviews =
+                List.of(
+                        buildReviewStub(1, EXPECTED_WORST_HOTEL_IDENTIFIER),
+                        buildReviewStub(2, EXPECTED_WORST_HOTEL_IDENTIFIER));
+
+        Hotel expectedIntermediateHotel = buildHotelStub(EXPECTED_INTERMEDIATE_HOTEL_IDENTIFIER);
+        List<Review> expectedIntermediateHotelReviews =
+                List.of(
+                        buildReviewStub(2, EXPECTED_INTERMEDIATE_HOTEL_IDENTIFIER),
+                        buildReviewStub(4, EXPECTED_INTERMEDIATE_HOTEL_IDENTIFIER));
+
+        Hotel expectedBestHotel = buildHotelStub(EXPECTED_BEST_HOTEL_IDENTIFIER);
+        List<Review> expectedBestHotelReviews =
+                List.of(
+                        buildReviewStub(4, EXPECTED_BEST_HOTEL_IDENTIFIER),
+                        buildReviewStub(5, EXPECTED_BEST_HOTEL_IDENTIFIER));
+
+        when(hotelRepository.findHotelsMatchingQuery(any(HotelQuery.class)))
+                .thenReturn(
+                        List.of(expectedIntermediateHotel, expectedWorstHotel, expectedBestHotel));
+
+        when(reviewRepository.findReviewsMatchingHotelIdentifier(EXPECTED_WORST_HOTEL_IDENTIFIER))
+                .thenReturn(expectedWorstHotelReviews);
+        when(reviewRepository.findReviewsMatchingHotelIdentifier(
+                        EXPECTED_INTERMEDIATE_HOTEL_IDENTIFIER))
+                .thenReturn(expectedIntermediateHotelReviews);
+        when(reviewRepository.findReviewsMatchingHotelIdentifier(EXPECTED_BEST_HOTEL_IDENTIFIER))
+                .thenReturn(expectedBestHotelReviews);
+
+        List<RatedHotel> result = testee.findHotelsMatchingQuery(new HotelQuery());
+
+        assertThat(result).hasSize(3);
+        assertThat(result.get(0).getIdentifier()).isEqualTo(EXPECTED_BEST_HOTEL_IDENTIFIER);
+        assertThat(result.get(1).getIdentifier()).isEqualTo(EXPECTED_INTERMEDIATE_HOTEL_IDENTIFIER);
+        assertThat(result.get(2).getIdentifier()).isEqualTo(EXPECTED_WORST_HOTEL_IDENTIFIER);
+    }
+
+    private Hotel buildHotelStub(String identifier) {
+        return new Hotel(
+                identifier,
+                EXPECTED_NAME,
+                EXPECTED_DESCRIPTION,
+                EXPECTED_LOCATION,
+                EXPECTED_TOTAL_PRICE,
+                EXPECTED_IMAGE_URL);
+    }
+
+    private Review buildReviewStub(int rating, String hotelIdentifier) {
+        return new Review(
+                EXPECTED_REVIEW_IDENTIFIER, rating, EXPECTED_REVIEW_COMMENT, hotelIdentifier);
     }
 }
