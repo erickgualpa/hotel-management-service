d6366cfb7be7998d97f89e96bbdbde97a1d6c30c
Gualpa, Erick <erick.gualpa@sky.uk>
2023-04-26 08:55:13 +0200

    Setup failing test for in-memory review repository

--- src/main/java/org/egualpam/services/hotel/rating/infrastructure/InMemoryReviewRepository.java
+++ src/main/java/org/egualpam/services/hotel/rating/infrastructure/InMemoryReviewRepository.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import org.egualpam.services.hotel.rating.application.ReviewRepository;
 import org.egualpam.services.hotel.rating.domain.HotelReview;
+import org.springframework.stereotype.Component;
 
+@Component
 public class InMemoryReviewRepository implements ReviewRepository {
 
     @Override
--- src/test/java/org/egualpam/services/hotel/rating/infrastructure/InMemoryReviewRepositoryTest.java
+++ src/test/java/org/egualpam/services/hotel/rating/infrastructure/InMemoryReviewRepositoryTest.java
@@ -1,3 +1,24 @@
 package org.egualpam.services.hotel.rating.infrastructure;
 
-class InMemoryReviewRepositoryTest {}
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.egualpam.services.hotel.rating.domain.HotelReview;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class InMemoryReviewRepositoryTest {
+
+    private InMemoryReviewRepository testee;
+
+    @BeforeEach
+    void setup() {
+        testee = new InMemoryReviewRepository();
+    }
+
+    @Test
+    void givenHotelIdentifier_matchingReviewsShouldBeReturned() {
+        List<HotelReview> result = testee.findReviewsMatchingHotelIdentifier("AMZ_HOTEL");
+        assertThat(result).isNotEmpty();
+    }
+}
