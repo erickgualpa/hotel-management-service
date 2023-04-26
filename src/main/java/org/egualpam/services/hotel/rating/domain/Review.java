ef390491756a751fe7d4037ee7614b37ffc550fa
Gualpa, Erick <erick.gualpa@sky.uk>
2023-04-26 09:02:36 +0200

    Create review datasource entity and setup some default reviews on repository

---
+++ src/main/java/org/egualpam/services/hotel/rating/domain/Review.java
@@ -0,0 +1,34 @@
+package org.egualpam.services.hotel.rating.domain;
+
+public class Review {
+
+    private String identifier;
+    private Integer rating;
+    private String comment;
+    private String hotelIdentifier;
+
+    public Review() {}
+
+    public Review(String identifier, Integer rating, String comment, String hotelIdentifier) {
+        this.identifier = identifier;
+        this.rating = rating;
+        this.comment = comment;
+        this.hotelIdentifier = hotelIdentifier;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public String getHotelIdentifier() {
+        return hotelIdentifier;
+    }
+}
--- src/main/java/org/egualpam/services/hotel/rating/infrastructure/InMemoryReviewRepository.java
+++ src/main/java/org/egualpam/services/hotel/rating/infrastructure/InMemoryReviewRepository.java
@@ -4,10 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 import org.egualpam.services.hotel.rating.application.ReviewRepository;
 import org.egualpam.services.hotel.rating.domain.HotelReview;
+import org.egualpam.services.hotel.rating.domain.Review;
 import org.springframework.stereotype.Component;
 
 @Component
 public class InMemoryReviewRepository implements ReviewRepository {
+
+    private static final List<Review> inMemoryReviews = new ArrayList<>();
+
+    static {
+        inMemoryReviews.add(new Review("AMZ_REVIEW", 5, "Eloquent comment", "AMZ_HOTEL"));
+    }
 
     @Override
     public List<HotelReview> findReviewsMatchingHotelIdentifier(String hotelIdentifier) {
