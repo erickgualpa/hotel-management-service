8e3ff112df6ec9b9ccfc35225b2b5ca5d7cbf406
Gualpa, Erick <erick.gualpa@sky.uk>
2023-04-26 12:20:34 +0200

    Add generic E2E test

--- src/test/java/org/egualpam/services/hotel/rating/HotelRatingServiceApplicationTests.java
+++ src/test/java/org/egualpam/services/hotel/rating/HotelRatingServiceApplicationTests.java
@@ -1,13 +1,46 @@
 package org.egualpam.services.hotel.rating;
 
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class HotelRatingServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired private MockMvc mockMvc;
 
+    @Test
+    void serviceShouldReturnMatchingHotelsForGivenQuery() throws Exception {
+        String request =
+                "{\n"
+                        + "    \"location\": \"Barcelona\",\n"
+                        + "    \"checkIn\": \"2023-06-24\",\n"
+                        + "    \"checkOut\": \"2023-06-28\",\n"
+                        + "    \"priceRange\": {\n"
+                        + "        \"begin\": 0,\n"
+                        + "        \"end\": 100\n"
+                        + "    }\n"
+                        + "}";
+
+        MvcResult result =
+                mockMvc.perform(
+                                post("/api/hotel/query")
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .content(request))
+                        .andExpect(status().isOk())
+                        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                        .andExpect(jsonPath("$.size()", is(2)))
+                        .andReturn();
+    }
 }
