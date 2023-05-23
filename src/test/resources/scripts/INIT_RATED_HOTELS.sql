e75efaeee2aaf9a0dc33ab901570503f35404e82
Gualpa, Erick <erick.gualpa@sky.uk>
2023-05-23 16:39:06 +0200

    Update database scripts

---
+++ src/test/resources/scripts/INIT_RATED_HOTELS.sql
@@ -0,0 +1,8 @@
+INSERT INTO hotels (id, name, description, location, total_price, image_url)
+VALUES (1, "Amazing hotel", "This is an amazing hotel", "Barcelona", 200, "amz-img-url.com");
+
+INSERT INTO hotels (id, name, description, location, total_price, image_url)
+VALUES (2, "Average hotel", "This is an average hotel", "Berlin", 100, "avg-img-url.com");
+
+INSERT INTO hotels (id, name, description, location, total_price, image_url)
+VALUES (3, "Basic hotel", "This is a basic hotel", "Barcelona", 50, "basic-img-url.com");
