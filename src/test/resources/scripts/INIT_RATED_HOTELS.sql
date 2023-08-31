3d80d4b751930eb0ae796305c7208955c9c4938f
Erick Gualpa <erickfabian.gualpa@adevinta.com>
2023-08-31 12:11:07 +0200

    Revert changes on db migration scripts

---
+++ src/test/resources/scripts/INIT_RATED_HOTELS.sql
@@ -0,0 +1,9 @@
+-- TODO: Review if this initialization is still needed or not
+INSERT INTO hotels (id, name, description, location, total_price, image_url)
+VALUES (1, "Amazing hotel", "This is an amazing hotel", "Barcelona", 200, "amz-img-url.com");
+
+INSERT INTO hotels (id, name, description, location, total_price, image_url)
+VALUES (2, "Average hotel", "This is an average hotel", "Berlin", 100, "avg-img-url.com");
+
+INSERT INTO hotels (id, name, description, location, total_price, image_url)
+VALUES (3, "Basic hotel", "This is a basic hotel", "Barcelona", 50, "basic-img-url.com");
