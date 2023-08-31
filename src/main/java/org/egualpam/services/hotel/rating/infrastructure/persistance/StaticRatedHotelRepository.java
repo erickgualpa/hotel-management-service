59d6d456d36c2d1650fea725f59503f844afc295
Erick Gualpa <erickfabian.gualpa@adevinta.com>
2023-08-31 11:34:43 +0200

    Amend StaticRatedHotelRepository

--- src/main/java/org/egualpam/services/hotel/rating/infrastructure/persistance/StaticRatedHotelRepository.java
+++ src/main/java/org/egualpam/services/hotel/rating/infrastructure/persistance/StaticRatedHotelRepository.java
@@ -2,44 +2,40 @@ package org.egualpam.services.hotel.rating.infrastructure.persistance;
 
 import lombok.RequiredArgsConstructor;
 import org.egualpam.services.hotel.rating.application.HotelQuery;
+import org.egualpam.services.hotel.rating.domain.Location;
 import org.egualpam.services.hotel.rating.domain.RatedHotel;
 import org.egualpam.services.hotel.rating.domain.RatedHotelRepository;
+import org.egualpam.services.hotel.rating.domain.Review;
 
-import java.util.Collections;
 import java.util.List;
 
 
 @RequiredArgsConstructor
 public class StaticRatedHotelRepository implements RatedHotelRepository {
 
-    // TODO: Uncomment once amended
-    /*private final StaticHotelRepository hotelRepository;
-    private final StaticReviewRepository reviewRepository;*/
-
     @Override
     public List<RatedHotel> findHotelsMatchingQuery(HotelQuery query) {
-        /*List<RatedHotel> hotels =
-                hotelRepository.findHotelsMatchingQuery(query).stream()
-                        .map(
-                                h ->
-                                        new RatedHotel(
-                                                h.getIdentifier(),
-                                                h.getName(),
-                                                h.getDescription(),
-                                                new Location(
-                                                        h.getLocation().getIdentifier(),
-                                                        h.getLocation().getName()),
-                                                h.getTotalPrice(),
-                                                h.getImageURL()))
-                        .collect(Collectors.toList());
-        hotels.forEach(h -> h.addReviews(findReviewsMatchingHotel(h)));
-        return hotels;*/
-        return Collections.emptyList();
-    }
 
-    /*private List<Review> findReviewsMatchingHotel(RatedHotel h) {
-        return reviewRepository.findReviewsMatchingHotelIdentifier(h.getIdentifier()).stream()
-                .map(r -> new Review(r.getIdentifier(), r.getRating(), r.getComment()))
-                .collect(Collectors.toList());
-    }*/
+        RatedHotel fakeRatedHotel = new RatedHotel(
+                "some-hotel-identifier",
+                "some-hotel-name",
+                "some-hotel-description",
+                new Location(
+                        "some-location-identifier",
+                        "some-location-name"),
+                100,
+                "some-image-url");
+
+        fakeRatedHotel.addReviews(
+                List.of(
+                        new Review(
+                                "some-review-identifier",
+                                3,
+                                "some-review-comment"
+                        )
+                )
+        );
+
+        return List.of(fakeRatedHotel);
+    }
 }
--- src/test/java/org/egualpam/services/hotel/rating/infrastructure/persistance/StaticRatedHotelRepositoryTest.java
+++ src/test/java/org/egualpam/services/hotel/rating/infrastructure/persistance/StaticRatedHotelRepositoryTest.java
@@ -1,27 +1,61 @@
 package org.egualpam.services.hotel.rating.infrastructure.persistance;
 
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.egualpam.services.hotel.rating.application.HotelQuery;
+import org.egualpam.services.hotel.rating.domain.RatedHotel;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-@ExtendWith(MockitoExtension.class)
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class StaticRatedHotelRepositoryTest {
 
-    // TODO: Uncomment once amended
     private StaticRatedHotelRepository testee;
-    /*
+
     @BeforeEach
     void setup() {
-        testee =
-                new StaticRatedHotelRepository(
-                        new StaticHotelRepository(), new StaticReviewRepository());
+        testee = new StaticRatedHotelRepository();
     }
 
     @Test
-    void givenAnyQuery_ratedHotelsShouldBeReturned() {
+    void givenAnyQuery_fakeRatedHotelShouldBeReturned() {
+
         List<RatedHotel> result = testee.findHotelsMatchingQuery(HotelQuery.create().build());
+
         assertThat(result)
-                .hasSize(5)
+                .hasSize(1)
                 .allSatisfy(
-                        actualRatedHotel -> assertThat(actualRatedHotel.getReviews()).isNotEmpty());
-    }*/
+                        actualRatedHotel -> {
+
+                            assertThat(actualRatedHotel.getIdentifier()).isEqualTo("some-hotel-identifier");
+                            assertThat(actualRatedHotel.getName()).isEqualTo("some-hotel-name");
+                            assertThat(actualRatedHotel.getDescription()).isEqualTo("some-hotel-description");
+
+                            assertThat(actualRatedHotel.getLocation())
+                                    .satisfies(actualLocation -> {
+
+                                        assertThat(actualLocation.getIdentifier())
+                                                .isEqualTo("some-location-identifier");
+
+                                        assertThat(actualLocation.getName()).isEqualTo("some-location-name");
+                                    });
+
+                            assertThat(actualRatedHotel.getTotalPrice()).isEqualTo(100);
+                            assertThat(actualRatedHotel.getImageURL()).isEqualTo("some-image-url");
+
+                            assertThat(actualRatedHotel.getReviews())
+                                    .hasSize(1)
+                                    .allSatisfy(actualReview -> {
+
+                                        assertThat(actualReview.getIdentifier())
+                                                .isEqualTo("some-review-identifier");
+
+                                        assertThat(actualReview.getRating()).isEqualTo(3);
+
+                                        assertThat(actualReview.getComment())
+                                                .isEqualTo("some-review-comment");
+                                    });
+                        });
+    }
 }
