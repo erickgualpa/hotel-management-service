c204f1d43b5a4da8e291676512a79b41db4bcde2
Erick Gualpa <erickfabian.gualpa@adevinta.com>
2023-09-01 12:41:56 +0200

    Add TODO

--- src/main/java/org/egualpam/services/hotel/rating/infrastructure/persistance/jpa/HotelCriteriaQueryBuilder.java
+++ src/main/java/org/egualpam/services/hotel/rating/infrastructure/persistance/jpa/HotelCriteriaQueryBuilder.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+// TODO: Rename this class
 public class HotelCriteriaQueryBuilder {
 
     private final EntityManager entityManager;
