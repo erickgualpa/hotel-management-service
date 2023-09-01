1a20fcb1489b7d291ef3927b68c02bb821c59b7f
Erick Gualpa <erickfabian.gualpa@adevinta.com>
2023-09-01 14:13:41 +0200

    Convert RatedHotelRepository into abstract class

--- src/main/java/org/egualpam/services/hotel/rating/infrastructure/persistance/jpa/PostgreSqlJpaHotelRepository.java
+++ src/main/java/org/egualpam/services/hotel/rating/infrastructure/persistance/jpa/PostgreSqlJpaHotelRepository.java
@@ -1,7 +1,6 @@
 package org.egualpam.services.hotel.rating.infrastructure.persistance.jpa;
 
 import org.egualpam.services.hotel.rating.application.HotelQuery;
-import org.egualpam.services.hotel.rating.domain.Location;
 import org.egualpam.services.hotel.rating.domain.RatedHotel;
 import org.egualpam.services.hotel.rating.domain.RatedHotelRepository;
 import org.egualpam.services.hotel.rating.infrastructure.persistance.HotelDto;
@@ -9,10 +8,9 @@ import org.egualpam.services.hotel.rating.infrastructure.persistance.HotelDto;
 import javax.persistence.EntityManager;
 import javax.persistence.criteria.CriteriaQuery;
 import java.util.List;
-import java.util.UUID;
 import java.util.stream.Collectors;
 
-public class PostgreSqlJpaHotelRepository implements RatedHotelRepository {
+public class PostgreSqlJpaHotelRepository extends RatedHotelRepository {
 
     private final EntityManager entityManager;
     private final HotelCriteriaQueryBuilder hotelCriteriaQueryBuilder;
@@ -30,20 +28,17 @@ public class PostgreSqlJpaHotelRepository implements RatedHotelRepository {
         return entityManager.createQuery(criteriaQuery)
                 .getResultList()
                 .stream()
-                .map(this::mapToEntity)
+                .map(
+                        hotelDto ->
+                                buildEntity(
+                                        hotelDto.id().toString(),
+                                        hotelDto.name(),
+                                        hotelDto.description(),
+                                        hotelDto.location(),
+                                        hotelDto.totalPrice(),
+                                        hotelDto.imageURL()
+                                )
+                )
                 .collect(Collectors.toList());
-    }
-
-    private RatedHotel mapToEntity(HotelDto hotelDto) {
-
-        return new RatedHotel(
-                hotelDto.id().toString(),
-                hotelDto.name(),
-                hotelDto.description(),
-                // TODO: Decide what how 'Location' will be managed (Entity or Value)
-                new Location(UUID.randomUUID().toString(), hotelDto.location()),
-                hotelDto.totalPrice(),
-                hotelDto.imageURL()
-        );
     }
 }
