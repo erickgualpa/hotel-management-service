4a78838cbb0848e45c9a27797da74207e197ac4e
Erick Gualpa <erickfabian.gualpa@adevinta.com>
2023-09-04 09:56:32 +0200

    Make Hotel domain entity class final

--- src/main/java/org/egualpam/services/hotel/rating/domain/Hotel.java
+++ src/main/java/org/egualpam/services/hotel/rating/domain/Hotel.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class Hotel {
+public final class Hotel {
 
     private final String identifier;
     private final String name;
