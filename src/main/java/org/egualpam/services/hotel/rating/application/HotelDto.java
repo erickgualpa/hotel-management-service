0d5b2b2e4be5def6912d4d91f762ce85ee382607
Erick Gualpa <erickfabian.gualpa@adevinta.com>
2023-09-07 23:18:33 +0200

    Update DTOs from application layer

--- src/main/java/org/egualpam/services/hotel/rating/application/HotelDto.java
+++ src/main/java/org/egualpam/services/hotel/rating/application/HotelDto.java
@@ -1,10 +1,13 @@
 package org.egualpam.services.hotel.rating.application;
 
+import java.util.List;
+
 public record HotelDto(
         String identifier,
         String name,
         String description,
         String location,
         Integer totalPrice,
-        String imageURL) {
+        String imageURL,
+        List<ReviewDto> reviews) {
 }
