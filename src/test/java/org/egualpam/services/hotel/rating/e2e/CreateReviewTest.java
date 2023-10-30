85f75a2023e034ec24d27a556fa95a9df343a96e
Erick Gualpa <erickfabian.gualpa@adevinta.com>
2023-10-30 17:03:14 +0100

    Create new class for review creation feature

---
+++ src/test/java/org/egualpam/services/hotel/rating/e2e/CreateReviewTest.java
@@ -0,0 +1,6 @@
+package org.egualpam.services.hotel.rating.e2e;
+
+import org.egualpam.services.hotel.rating.AbstractIntegrationTest;
+
+public class CreateReviewTest extends AbstractIntegrationTest {
+}
