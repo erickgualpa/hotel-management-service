d7d67e4a990224a87fd6323be701dec4d34f8440
Erick Gualpa <erickfabian.gualpa@adevinta.com>
2023-11-05 16:55:04 +0100

    Return bad request when creating a review with an invalid rating value

---
+++ src/main/java/org/egualpam/services/hotel/rating/application/reviews/InvalidRating.java
@@ -0,0 +1,4 @@
+package org.egualpam.services.hotel.rating.application.reviews;
+
+public class InvalidRating extends RuntimeException {
+}
--- src/main/java/org/egualpam/services/hotel/rating/infrastructure/controller/ReviewController.java
+++ src/main/java/org/egualpam/services/hotel/rating/infrastructure/controller/ReviewController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.egualpam.services.hotel.rating.application.reviews.CreateReview;
 import org.egualpam.services.hotel.rating.application.reviews.CreateReviewCommand;
 import org.egualpam.services.hotel.rating.application.reviews.FindReviewsByHotelIdentifier;
+import org.egualpam.services.hotel.rating.application.reviews.InvalidRating;
 import org.egualpam.services.hotel.rating.application.reviews.ReviewDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,22 +29,25 @@ public class ReviewController {
 
     @GetMapping
     public ResponseEntity<List<ReviewDto>> findReviewsByHotelIdentifier(@RequestParam String hotelIdentifier) {
-        return ResponseEntity.ok(
-                findReviewsByHotelIdentifier.execute(hotelIdentifier)
-        );
+        List<ReviewDto> reviews = findReviewsByHotelIdentifier.execute(hotelIdentifier);
+        return ResponseEntity.ok(reviews);
     }
 
     @PostMapping(path = "/{reviewIdentifier}")
     public ResponseEntity<Void> createReview(@PathVariable String reviewIdentifier,
                                              @RequestBody CreateReviewRequest createReviewRequest) {
-        createReview.execute(
-                new CreateReviewCommand(
-                        reviewIdentifier,
-                        createReviewRequest.hotelIdentifier(),
-                        createReviewRequest.rating(),
-                        createReviewRequest.comment()
-                )
+        CreateReviewCommand createReviewCommand = new CreateReviewCommand(
+                reviewIdentifier,
+                createReviewRequest.hotelIdentifier(),
+                createReviewRequest.rating(),
+                createReviewRequest.comment()
         );
+
+        try {
+            createReview.execute(createReviewCommand);
+        } catch (InvalidRating e) {
+            return ResponseEntity.badRequest().build();
+        }
 
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
