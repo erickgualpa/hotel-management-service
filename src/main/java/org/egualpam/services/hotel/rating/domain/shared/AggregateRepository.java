2415373e1f06dfa22408199e2a57a419bf478bb0
Erick Gualpa <erickfabian.gualpa@adevinta.com>
2024-03-05 23:14:56 +0100

    Add missing TODO

--- src/main/java/org/egualpam/services/hotel/rating/domain/shared/AggregateRepository.java
+++ src/main/java/org/egualpam/services/hotel/rating/domain/shared/AggregateRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface AggregateRepository<T extends AggregateRoot> {
+    // TODO: Add integration tests where required
     Optional<T> find(AggregateId id);
 
     List<T> find(Criteria criteria);
