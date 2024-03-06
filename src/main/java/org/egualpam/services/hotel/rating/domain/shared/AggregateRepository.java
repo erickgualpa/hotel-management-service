9c8ff45dc6d3df3e9a1442905949275195df3e7e
Erick Gualpa <64378740+erickgualpa@users.noreply.github.com>
2024-03-06 19:08:39 +0100

    Remove unneeded comment (#60)

--- src/main/java/org/egualpam/services/hotel/rating/domain/shared/AggregateRepository.java
+++ src/main/java/org/egualpam/services/hotel/rating/domain/shared/AggregateRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface AggregateRepository<T extends AggregateRoot> {
-    // TODO: Add integration tests where required
     Optional<T> find(AggregateId id);
 
     List<T> find(Criteria criteria);
