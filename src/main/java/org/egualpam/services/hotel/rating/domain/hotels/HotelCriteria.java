5761977ff6be9996d78371a512592e9ec2b57f30
Erick Gualpa <64378740+erickgualpa@users.noreply.github.com>
2024-03-15 19:30:43 +0100

    Use ViewSupplier in FindHotel use case (#63)

--- src/main/java/org/egualpam/services/hotel/rating/domain/hotels/HotelCriteria.java
+++ src/main/java/org/egualpam/services/hotel/rating/domain/hotels/HotelCriteria.java
@@ -1,16 +1,32 @@
 package org.egualpam.services.hotel.rating.domain.hotels;
 
+import org.egualpam.services.hotel.rating.domain.shared.AggregateId;
 import org.egualpam.services.hotel.rating.domain.shared.Criteria;
 
 import java.util.Optional;
 
 public final class HotelCriteria implements Criteria {
+    private final Optional<AggregateId> hotelId;
     private final Optional<Location> location;
     private final PriceRange priceRange;
 
-    public HotelCriteria(Optional<Location> location, PriceRange priceRange) {
+    // TODO: Split this into two different Criteria classes to avoid Optional parameters
+    public HotelCriteria(AggregateId hotelId) {
+        this.hotelId = Optional.of(hotelId);
+        this.location = Optional.empty();
+        this.priceRange = new PriceRange(
+                Optional.empty(),
+                Optional.empty()
+        );
+    }
+
+    public HotelCriteria(
+            Optional<Location> location,
+            PriceRange priceRange
+    ) {
         this.location = location;
         this.priceRange = priceRange;
+        this.hotelId = Optional.empty();
     }
 
     public Optional<Location> getLocation() {
@@ -19,5 +35,9 @@ public final class HotelCriteria implements Criteria {
 
     public PriceRange getPriceRange() {
         return priceRange;
+    }
+
+    public Optional<AggregateId> getHotelId() {
+        return hotelId;
     }
 }
