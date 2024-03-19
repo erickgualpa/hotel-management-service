f1d2b60b9a715a2b9605619e6cf49d752b201704
Erick Gualpa <erickfabian.gualpa@adevinta.com>
2024-03-19 20:00:52 +0100

    Implement ViewSupplier<HotelView>

---
+++ src/main/java/org/egualpam/services/hotelmanagement/infrastructure/persistence/jpa/PostgreSqlJpaHotelViewSupplier.java
@@ -0,0 +1,53 @@
+package org.egualpam.services.hotelmanagement.infrastructure.persistence.jpa;
+
+import jakarta.persistence.EntityManager;
+import org.egualpam.services.hotelmanagement.application.hotels.HotelView;
+import org.egualpam.services.hotelmanagement.application.shared.ViewSupplier;
+import org.egualpam.services.hotelmanagement.domain.hotels.HotelCriteria;
+import org.egualpam.services.hotelmanagement.domain.shared.AggregateId;
+import org.egualpam.services.hotelmanagement.domain.shared.Criteria;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Function;
+
+public class PostgreSqlJpaHotelViewSupplier implements ViewSupplier<HotelView> {
+
+    private final EntityManager entityManager;
+    private final Function<PersistenceHotel, List<PersistenceReview>> findReviewsByHotel;
+
+    public PostgreSqlJpaHotelViewSupplier(EntityManager entityManager) {
+        this.entityManager = entityManager;
+        this.findReviewsByHotel = new FindReviewsByHotel(entityManager);
+    }
+
+    @Override
+    public HotelView get(Criteria criteria) {
+        AggregateId hotelId = ((HotelCriteria) criteria).getHotelId().orElseThrow();
+        PersistenceHotel persistenceHotel = entityManager.find(PersistenceHotel.class, hotelId.value());
+        return Optional.ofNullable(persistenceHotel)
+                .map(this::mapIntoViewHotel)
+                .map(HotelView::new)
+                .orElseThrow();
+    }
+
+    private HotelView.Hotel mapIntoViewHotel(PersistenceHotel persistenceHotel) {
+        double averageRating = findReviewsByHotel
+                .apply(persistenceHotel)
+                .stream()
+                .mapToDouble(PersistenceReview::getRating)
+                .filter(Objects::nonNull)
+                .average()
+                .orElse(0.0);
+        return new HotelView.Hotel(
+                persistenceHotel.getId().toString(),
+                persistenceHotel.getName(),
+                persistenceHotel.getDescription(),
+                persistenceHotel.getLocation(),
+                persistenceHotel.getTotalPrice(),
+                persistenceHotel.getImageURL(),
+                averageRating
+        );
+    }
+}
