6b4f5045a05345460b5bdc5c4147ab9d68798827
Erick Gualpa <erickfabian.gualpa@adevinta.com>
2024-03-21 18:42:23 +0100

    Remove no longer needed classes

--- src/main/java/org/egualpam/services/hotelmanagement/infrastructure/cqrs/simple/SimpleQueryBus.java
+++ src/main/java/org/egualpam/services/hotelmanagement/infrastructure/cqrs/simple/SimpleQueryBus.java
@@ -1,7 +1,6 @@
 package org.egualpam.services.hotelmanagement.infrastructure.cqrs.simple;
 
 import lombok.RequiredArgsConstructor;
-import org.egualpam.services.hotelmanagement.application.hotels.FindHotel;
 import org.egualpam.services.hotelmanagement.application.hotels.FindHotels;
 import org.egualpam.services.hotelmanagement.application.hotels.HotelView;
 import org.egualpam.services.hotelmanagement.application.hotels.HotelsView;
@@ -12,6 +11,8 @@ import org.egualpam.services.hotelmanagement.application.shared.Query;
 import org.egualpam.services.hotelmanagement.application.shared.QueryBus;
 import org.egualpam.services.hotelmanagement.application.shared.View;
 import org.egualpam.services.hotelmanagement.application.shared.ViewSupplier;
+import org.egualpam.services.hotelmanagement.domain.hotels.HotelCriteria;
+import org.egualpam.services.hotelmanagement.domain.shared.AggregateId;
 
 import java.util.Map;
 
@@ -89,12 +90,12 @@ public final class SimpleQueryBus implements QueryBus {
 
         @Override
         public View handle(Query query) {
-            InternalQuery<HotelView> internalQuery =
-                    new FindHotel(
-                            ((FindHotelQuery) query).getHotelId(),
-                            hotelViewSupplier
-                    );
-            return internalQuery.get();
+            String hotelId = ((FindHotelQuery) query).getHotelId();
+            return hotelViewSupplier.get(
+                    new HotelCriteria(
+                            new AggregateId(hotelId)
+                    )
+            );
         }
     }
 }
