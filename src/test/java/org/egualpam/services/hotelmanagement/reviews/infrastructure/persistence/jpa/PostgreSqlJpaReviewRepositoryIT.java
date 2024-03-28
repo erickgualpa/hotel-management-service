ee023f48de179e3cc25da6b98ae1b45ea2c3232a
Erick Gualpa <erickfabian.gualpa@adevinta.com>
2024-03-28 15:00:00 +0100

    Move integration test into reviews module

--- src/test/java/org/egualpam/services/hotelmanagement/reviews/infrastructure/persistence/jpa/PostgreSqlJpaReviewRepositoryIT.java
+++ src/test/java/org/egualpam/services/hotelmanagement/reviews/infrastructure/persistence/jpa/PostgreSqlJpaReviewRepositoryIT.java
@@ -1,10 +1,9 @@
-package org.egualpam.services.hotelmanagement.shared.infrastructure.persistence.jpa;
+package org.egualpam.services.hotelmanagement.reviews.infrastructure.persistence.jpa;
 
 import jakarta.persistence.EntityManager;
 import org.egualpam.services.hotelmanagement.domain.reviews.Review;
 import org.egualpam.services.hotelmanagement.domain.shared.AggregateId;
 import org.egualpam.services.hotelmanagement.domain.shared.AggregateRepository;
-import org.egualpam.services.hotelmanagement.reviews.infrastructure.persistence.jpa.PostgreSqlJpaReviewRepository;
 import org.egualpam.services.hotelmanagement.shared.infrastructure.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
