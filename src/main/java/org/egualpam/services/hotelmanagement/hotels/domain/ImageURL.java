217fd13af8eb29b7e2dae8cf5bb2562a475c770e
Erick Gualpa <64378740+erickgualpa@users.noreply.github.com>
2024-03-31 20:15:02 +0200

    Add some TODOs (#82)

--- src/main/java/org/egualpam/services/hotelmanagement/hotels/domain/ImageURL.java
+++ src/main/java/org/egualpam/services/hotelmanagement/hotels/domain/ImageURL.java
@@ -1,4 +1,5 @@
 package org.egualpam.services.hotelmanagement.hotels.domain;
 
+// TODO: Replace this value object by an actual implementation of URLs validation
 public record ImageURL(String value) {
 }
--- src/main/java/org/egualpam/services/hotelmanagement/hotels/domain/Location.java
+++ src/main/java/org/egualpam/services/hotelmanagement/hotels/domain/Location.java
@@ -1,4 +1,5 @@
 package org.egualpam.services.hotelmanagement.hotels.domain;
 
+// TODO: Replace this by just the city where the hotel is located
 public record Location(String value) {
 }
