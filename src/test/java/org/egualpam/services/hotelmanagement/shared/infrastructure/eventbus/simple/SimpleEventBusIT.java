7188ab846d39bcfb6a55c95b4033eb364738b920
Erick Gualpa <erickfabian.gualpa@adevinta.com>
2024-05-29 16:37:10 +0200

    Amend assertion

--- src/test/java/org/egualpam/services/hotelmanagement/shared/infrastructure/eventbus/simple/SimpleEventBusIT.java
+++ src/test/java/org/egualpam/services/hotelmanagement/shared/infrastructure/eventbus/simple/SimpleEventBusIT.java
@@ -18,6 +18,7 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @Transactional
 class SimpleEventBusIT extends AbstractIntegrationTest {
@@ -50,7 +51,7 @@ class SimpleEventBusIT extends AbstractIntegrationTest {
         assertThat(result).satisfies(r -> {
             assertThat(r.type()).isEqualTo("hotelmanagement.reviews.created.v1.0");
             assertThat(r.aggregateId()).isEqualTo(aggregateId);
-            assertThat(r.occurredOn()).isEqualTo(occurredOn);
+            assertNotNull(r.occurredOn());
         });
     }
 }
