dc6368d69ab584c6199dab6f58ba20687c1ba7e7
Erick Gualpa <erickfabian.gualpa@adevinta.com>
2024-05-30 16:43:01 +0200

    Move RabbitMQ configuration into SharedConfiguration

--- src/main/java/org/egualpam/services/hotelmanagement/shared/infrastructure/eventbus/rabbitmq/RabbitMqEventBus.java
+++ src/main/java/org/egualpam/services/hotelmanagement/shared/infrastructure/eventbus/rabbitmq/RabbitMqEventBus.java
@@ -4,10 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
 import org.egualpam.services.hotelmanagement.shared.domain.DomainEvent;
 import org.egualpam.services.hotelmanagement.shared.domain.EventBus;
-import org.egualpam.services.hotelmanagement.shared.infrastructure.configuration.properties.eventbus.RabbitMqProperties;
 import org.egualpam.services.hotelmanagement.shared.infrastructure.eventbus.events.PublicEvent;
 import org.egualpam.services.hotelmanagement.shared.infrastructure.eventbus.events.PublicEventFactory;
 import org.slf4j.Logger;
@@ -22,45 +20,28 @@ public final class RabbitMqEventBus implements EventBus {
 
     private final Logger logger = LoggerFactory.getLogger(RabbitMqEventBus.class);
 
-    private final Connection rabbitMqConnection;
+    private final Connection connection;
     private final ObjectMapper objectMapper;
 
-    public RabbitMqEventBus(
-            RabbitMqProperties rabbitMqProperties,
-            ObjectMapper objectMapper
-    ) {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost(rabbitMqProperties.getHost());
-        factory.setPort(rabbitMqProperties.getAmqpPort());
-        factory.setUsername(rabbitMqProperties.getAdminUsername());
-        factory.setPassword(rabbitMqProperties.getAdminPassword());
-
-        logger.info("Connecting to RabbitMQ at {}:{}", rabbitMqProperties.getHost(), rabbitMqProperties.getAmqpPort());
-
-        try {
-            // TODO: Consider using a connection pool
-            this.rabbitMqConnection = factory.newConnection();
-        } catch (IOException | TimeoutException e) {
-            throw new RuntimeException("Connection could not be established", e);
-        }
-
+    public RabbitMqEventBus(Connection connection, ObjectMapper objectMapper) {
+        this.connection = connection;
         this.objectMapper = objectMapper;
     }
 
     @Override
     public void publish(List<DomainEvent> events) {
-        try (Channel channel = rabbitMqConnection.createChannel()) {
+        List<PublicEvent> publicEvents = events.stream().map(PublicEventFactory::from).toList();
+        try (Channel channel = connection.createChannel()) {
             // TODO: Queues should be already configured
             channel.queueDeclare("hotelmanagement.reviews", false, false, false, null);
-            events.forEach(e -> publishEvent(e, channel));
+            publicEvents.forEach(e -> publishEvent(e, channel));
         } catch (IOException | TimeoutException e) {
             // TODO: Consider using a custom exception
             throw new RuntimeException(e);
         }
     }
 
-    private void publishEvent(DomainEvent domainEvent, Channel channel) {
-        PublicEvent publicEvent = PublicEventFactory.from(domainEvent);
+    private void publishEvent(PublicEvent publicEvent, Channel channel) {
         try {
             byte[] serializedEvent = objectMapper.writeValueAsBytes(publicEvent);
             channel.basicPublish("", "hotelmanagement.reviews", null, serializedEvent);
