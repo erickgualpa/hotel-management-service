67cf8dd1fd271a3fe6a22af27342156157970c28
Erick Gualpa <m.erickgualpa@gmail.com>
2024-08-11 12:39:22 +0200

    Add TODO

--- src/main/java/org/egualpam/services/hotelmanagement/shared/application/query/View.java
+++ src/main/java/org/egualpam/services/hotelmanagement/shared/application/query/View.java
@@ -1,4 +1,5 @@
 package org.egualpam.services.hotelmanagement.shared.application.query;
 
+// TODO: Rename implementations of this in order find a better distinction between single and collections
 public interface View {
 }
