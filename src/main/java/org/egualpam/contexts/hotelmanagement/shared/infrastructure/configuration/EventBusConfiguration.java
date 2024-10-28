1fcf9a68a6f4f173d15d4de349ac96303d4a7ba4
Erick Gualpa <m.erickgualpa@gmail.com>
2024-10-28 14:05:01 +0100

    Add missing annotation

--- src/main/java/org/egualpam/contexts/hotelmanagement/shared/infrastructure/configuration/EventBusConfiguration.java
+++ src/main/java/org/egualpam/contexts/hotelmanagement/shared/infrastructure/configuration/EventBusConfiguration.java
@@ -11,11 +11,13 @@ import org.egualpam.contexts.hotelmanagement.shared.infrastructure.eventbus.rabb
 import org.egualpam.contexts.hotelmanagement.shared.infrastructure.eventbus.simple.SimpleEventBus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 @Configuration
+@EnableConfigurationProperties(RabbitMqProperties.class)
 public class EventBusConfiguration {
 
   private final Logger logger = LoggerFactory.getLogger(this.getClass());
