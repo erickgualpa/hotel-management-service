0fce6675709b30514dd377d4cb49f28848763bbe
Erick Gualpa <m.erickgualpa@gmail.com>
2025-02-02 13:32:32 +0100

    Remove UpdateHotelRating use case from hotel module

--- src/main/java/org/egualpam/contexts/hotelmanagement/hotel/infrastructure/configuration/HotelApplicationConfiguration.java
+++ src/main/java/org/egualpam/contexts/hotelmanagement/hotel/infrastructure/configuration/HotelApplicationConfiguration.java
@@ -2,14 +2,12 @@ package org.egualpam.contexts.hotelmanagement.hotel.infrastructure.configuration
 
 import java.time.Clock;
 import org.egualpam.contexts.hotelmanagement.hotel.application.command.CreateHotel;
-import org.egualpam.contexts.hotelmanagement.hotel.application.command.UpdateHotelRating;
 import org.egualpam.contexts.hotelmanagement.hotel.application.query.FindHotel;
 import org.egualpam.contexts.hotelmanagement.hotel.application.query.FindHotels;
 import org.egualpam.contexts.hotelmanagement.hotel.application.query.ManyHotels;
 import org.egualpam.contexts.hotelmanagement.hotel.application.query.OneHotel;
 import org.egualpam.contexts.hotelmanagement.hotel.domain.Hotel;
 import org.egualpam.contexts.hotelmanagement.hotel.domain.HotelCriteria;
-import org.egualpam.contexts.hotelmanagement.hotel.domain.ReviewIsAlreadyProcessed;
 import org.egualpam.contexts.hotelmanagement.hotel.domain.UniqueHotelCriteria;
 import org.egualpam.contexts.hotelmanagement.shared.application.query.ReadModelSupplier;
 import org.egualpam.contexts.hotelmanagement.shared.domain.AggregateRepository;
@@ -40,16 +38,5 @@ public class HotelApplicationConfiguration {
       AggregateRepository<Hotel> hotelRepository,
       EventBus eventBus) {
     return new CreateHotel(clock, uniqueIdSupplier, hotelRepository, eventBus);
-  }
-
-  @Bean
-  public UpdateHotelRating updateHotelRating(
-      Clock clock,
-      UniqueIdSupplier uniqueIdSupplier,
-      AggregateRepository<Hotel> hotelRepository,
-      ReviewIsAlreadyProcessed reviewIsAlreadyProcessed,
-      EventBus eventBus) {
-    return new UpdateHotelRating(
-        clock, uniqueIdSupplier, hotelRepository, reviewIsAlreadyProcessed, eventBus);
   }
 }
