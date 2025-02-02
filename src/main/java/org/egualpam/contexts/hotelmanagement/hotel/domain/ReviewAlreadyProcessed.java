4f26d969a6bedaea6347ffaaa7b5523792a66ca1
Erick Gualpa <m.erickgualpa@gmail.com>
2025-02-02 16:34:25 +0100

    Remove unneeded exception

--- src/main/java/org/egualpam/contexts/hotelmanagement/hotel/domain/ReviewAlreadyProcessed.java
+++
@@ -1,9 +0,0 @@
-package org.egualpam.contexts.hotelmanagement.hotel.domain;
-
-import org.egualpam.contexts.hotelmanagement.shared.domain.EntityId;
-
-public class ReviewAlreadyProcessed extends RuntimeException {
-  public ReviewAlreadyProcessed(EntityId reviewId) {
-    super("Review with id [%s] was already processed".formatted(reviewId.value()));
-  }
-}
