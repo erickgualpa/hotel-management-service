b9f104640cf845b08131dba6f6ccfa1770f830dd
Erick Gualpa <m.erickgualpa@gmail.com>
2025-07-29 23:17:01 +0200

    Add constraint for room

--- src/main/resources/db/migration/V0007__CREATE_ROOM_TABLE.sql
+++ src/main/resources/db/migration/V0007__CREATE_ROOM_TABLE.sql
@@ -1,7 +1,7 @@
 CREATE TABLE room
 (
     id              UUID NOT NULL,
-    hotel_id        UUID NOT NULL,
+    hotel_id        UUID NOT NULL REFERENCES hotels(id),
     room_type       VARCHAR(5) NOT NULL,
     PRIMARY KEY (id)
 );
--- src/test/java/org/egualpam/contexts/hotelmanagement/e2e/CreateRoomFeature.java
+++ src/test/java/org/egualpam/contexts/hotelmanagement/e2e/CreateRoomFeature.java
@@ -1,6 +1,7 @@
 package org.egualpam.contexts.hotelmanagement.e2e;
 
 import static java.util.UUID.randomUUID;
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -8,6 +9,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.util.UUID;
 import org.egualpam.contexts.hotelmanagement.shared.infrastructure.AbstractIntegrationTest;
+import org.egualpam.contexts.hotelmanagement.shared.infrastructure.helpers.HotelTestRepository;
 import org.egualpam.contexts.hotelmanagement.shared.infrastructure.helpers.RoomTestRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,19 +18,29 @@ class CreateRoomFeature extends AbstractIntegrationTest {
 
   private static final String CREATE_ROOM_REQUEST =
       """
-            {
-                "id": "%s",
-                "type": "%s",
-                "hotelId": "%s"
-            }
-            """;
+      {
+          "id": "%s",
+          "type": "%s",
+          "hotelId": "%s"
+      }
+      """;
 
+  @Autowired private HotelTestRepository hotelTestRepository;
   @Autowired private RoomTestRepository roomTestRepository;
 
   @Test
   void roomShouldBeCreated() throws Exception {
     UUID roomId = randomUUID();
+
     UUID hotelId = randomUUID();
+    String hotelName = randomAlphabetic(5);
+    String hotelDescription = randomAlphabetic(10);
+    String hotelLocation = randomAlphabetic(10);
+    Integer hotelPrice = 100;
+    String hotelImageURL = "www." + randomAlphabetic(5) + ".com";
+
+    hotelTestRepository.insertHotel(
+        hotelId, hotelName, hotelDescription, hotelLocation, hotelPrice, hotelImageURL);
 
     String request = String.format(CREATE_ROOM_REQUEST, roomId, "S", hotelId);
 
