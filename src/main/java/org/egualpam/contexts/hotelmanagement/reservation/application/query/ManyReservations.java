28e721675889984f5d29484556748761a4bf89db
Erick Gualpa <m.erickgualpa@gmail.com>
2025-08-02 13:20:15 +0200

    Remove unused class

--- src/main/java/org/egualpam/contexts/hotelmanagement/reservation/application/query/ManyReservations.java
+++
@@ -1,5 +0,0 @@
-package org.egualpam.contexts.hotelmanagement.reservation.application.query;
-
-import org.egualpam.contexts.hotelmanagement.shared.application.query.ReadModel;
-
-public record ManyReservations() implements ReadModel {}
